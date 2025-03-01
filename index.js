a840a676450cb6b67edc379dac664b945f76f2d1
toymurrahman <rahmantoymur2@gmail.com>
2025-03-01 08:45:25 +0600

    Delete from DB

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -37,14 +37,19 @@ async function run() {
       const cursor = coffeeCollection.find();
       const coffee = await cursor.toArray();
       res.send(coffee);
-
-    }
-  )
+    });
 
     app.post("/coffee", async(req, res) => {
       const newCoffee = req.body;
       console.log(newCoffee);
       const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    });
+
+    app.delete("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeeCollection.deleteOne(query);
       res.send(result);
     });
 
