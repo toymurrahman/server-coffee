516d138c5c4e9ab8e1eeb5578da0d14c483fd588
toymurrahman <rahmantoymur2@gmail.com>
2025-03-02 22:02:45 +0600

    deployed in vercel

--- index.js
+++ index.js
@@ -30,8 +30,12 @@ async function run() {
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
 
+    // create coffeeBd and coffee in the database
     const database = client.db("coffeeDB");
     const coffeeCollection = database.collection("coffee");
+
+    // create users in coffeeDB 
+    const userCollection = client.db("coffeeDB").collection("users");
 
     app.get('/coffee', async (req, res) => {
       const cursor = coffeeCollection.find();
@@ -64,6 +68,45 @@ async function run() {
       const query = {_id: new ObjectId(id)}
       const result = await coffeeCollection.deleteOne(query);
       res.send(result);
+    });
+
+    // user related functions
+    app.get('/users', async (req, res) => {
+      const cursor = userCollection.find();
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const user = await userCollection.findOne(query);
+      res.send(user);
+    });
+
+    app.patch("/users", async (req, res) => {
+      const email = req.body.email;
+      const filter = {email};
+     const updatedDoc = 
+     {$set:
+       {
+        lastSignInTime: req.body?.lastSignInTime
+       }
+      }
+      const result = await userCollection.updateOne(filter,updatedDoc);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+     const id = req.params.id;
+     const query = {_id: new ObjectId(id)};
+     const result = await userCollection.deleteOne(query);
+     res.send(result);
     });
 
     // Send a ping to confirm a successful connection
